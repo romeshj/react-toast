32f57fadf3b4f182a40aa0c3d237407b3607e62f
romeshj <romedberg@gmail.com>
2021-03-03 20:47:24 +0530

    React Toast Component using scss and create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Toast from './components/toast/Toast';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+<i class="fas fa-exclamation-triangle"></i>
 
-function App() {
+const successIcon = <FontAwesomeIcon icon={faCheckCircle} style={{ width: '30px', height: '30px' }} />
+const warningIcon = <FontAwesomeIcon icon={faExclamationTriangle} style={{ width: '30px', height: '30px' }} />
+
+const App = () => {
+
+  const successToast = {
+    id: 1,
+    title: 'Success',
+    description: 'This is a success toast component',
+    type: 'success',
+    icon: successIcon
+  }
+
+  const warningToast = {
+    id: 1,
+    title: 'Warning',
+    description: 'This is a warning toast component',
+    type: 'warning',
+    icon: warningIcon
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Toast toastConfig={successToast} position="bottom-right" />
+      <Toast toastConfig={warningToast} position="bottom-left" />
     </div>
   );
 }
