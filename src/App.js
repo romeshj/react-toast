ee4d1052c53b4d1a8bbb7efd367b67c910d7e899
romeshj <romedberg@gmail.com>
2021-03-04 11:22:01 +0530

    Added two more taost component - Info and Error

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
 import './App.scss';
 import Toast from './components/toast/Toast';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-<i class="fas fa-exclamation-triangle"></i>
+import { faCheckCircle, faExclamationTriangle, faExclamationCircle, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 const successIcon = <FontAwesomeIcon icon={faCheckCircle} style={{ width: '30px', height: '30px' }} />
 const warningIcon = <FontAwesomeIcon icon={faExclamationTriangle} style={{ width: '30px', height: '30px' }} />
+const errorIcon = <FontAwesomeIcon icon={faExclamationCircle} style={{ width: '30px', height: '30px' }} />
+const infoIcon = <FontAwesomeIcon icon={faInfoCircle} style={{ width: '30px', height: '30px' }} />
 
 const App = () => {
 
   const successToast = {
-    id: 1,
     title: 'Success',
     description: 'This is a success toast component',
     type: 'success',
-    icon: successIcon
+    icon: successIcon,
+    show: true,
   }
 
   const warningToast = {
-    id: 1,
     title: 'Warning',
     description: 'This is a warning toast component',
     type: 'warning',
-    icon: warningIcon
+    icon: warningIcon,
+    show: true,
+  }
+
+  const errorToast = {
+    title: 'Error',
+    description: 'This is a error toast component',
+    type: 'error',
+    icon: errorIcon,
+    show: true,
+  }
+
+  const infoToast = {
+    title: 'Info',
+    description: 'This is a info toast component',
+    type: 'info',
+    icon: infoIcon,
+    show: true,
   }
 
   return (
     <div className="app">
       <Toast toastConfig={successToast} position="bottom-right" />
       <Toast toastConfig={warningToast} position="bottom-left" />
+      <Toast toastConfig={errorToast} position="top-left" />
+      <Toast toastConfig={infoToast} position="top-right" />
     </div>
   );
 }
--- src/components/toast/Toast.js
+++ src/components/toast/Toast.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./Toast.scss";
 
 const Toast = ({ toastConfig, position }) => {
+
+    const [visible, setVisible] = useState(toastConfig.show);
+
+    const closeToaster = () => {
+        setVisible(false);
+    }
+
     return (
-        <div className={`notification-container ${position}`}>
-            <div className={`notification toast ${toastConfig.type}`}>
-                <button>
-                    X
-                </button>
-                <div className="notification-image">
-                    {toastConfig.icon}
+        <>
+            {visible &&
+
+                <div className={`notification-container ${position}`}>
+                    <div className={`notification toast ${toastConfig.type}`}>
+                        <button onClick={() => { closeToaster() }}>X</button>
+                        <div className="notification-image">
+                            {toastConfig.icon}
+                        </div>
+                        <div>
+                            <p className="notification-title">{toastConfig.title}</p>
+                            <p className="notification-message">{toastConfig.description}</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <p className="notification-title">{toastConfig.title}</p>
-                    <p className="notification-message">{toastConfig.description}</p>
-                </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 export default Toast;
